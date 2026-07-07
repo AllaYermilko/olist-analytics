ce19bfeae10de099220032587602d8225a11cf25
AllaYermilko <alla.yermilko@gmail.com>
2026-07-07 15:17:42 +0300

    Update queries.sql

--- sql/queries.sql
+++ sql/queries.sql
@@ -1,3 +1,4 @@
+--Головний датасет: позиції доставлених замовлень з контекстом
 SELECT 
 	o.order_id,
     o.order_purchase_t,
@@ -16,3 +17,15 @@ LEFT JOIN product_category_name_translation t USING (product_category)
 LEFT JOIN olist_order_payments_dataset op USING (order_id)
 LEFT JOIN olist_order_reviews_dataset r USING (order_id)
 WHERE o.order_status = 'delivered';
+
+--Місячний виторг і кількість замовлень
+SELECT
+	strftime('%Y-%m', o.order_purchase_t) as ym,
+    ROUND(SUM(oi.price), 2) as revenue,
+    COUNT(DISTINCT o.order_id) as orders
+from olist_orders_dataset o
+JOIN olist_order_items_dataset oi USING (order_id)
+WHERE o.order_status = 'delivered'
+GROUP by ym
+ORDER by ym;
+
